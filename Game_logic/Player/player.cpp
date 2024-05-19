ef7614fb638aece17834581800a4648ebb45ef4d
ITIvanov18 <ITIvanov18@codingburgas.bg>
2024-05-20 00:43:19 +0300

    Update Game_logic/Player settings

--- Game_logic/Player/player.cpp
+++ Game_logic/Player/player.cpp
@@ -1,16 +1,24 @@
 #include "player.h"
 #include <iostream>
+#include <limits.h>
 
 using namespace std;
 
 char switchPlayer(char currentPlayer) {
+    cout << "Switch turns" << endl;
     return currentPlayer == 'X' ? 'O' : 'X';
 }
 
 void makeMove(char board[3][3], char currentPlayer) {
     int choice;
     cout << "Enter the number of the cell: ";
-    cin >> choice; // TODO: add some validation
+
+    while (!(cin >> choice) || choice < 1 || choice > 9) {
+		cin.clear(); // Изчиства грешките от потока
+		cin.ignore(INT_MAX, '\n'); // Игнорира грешния вход
+		cout << "Invalid cell! You can choose between the numbers 1 and 9."<< endl;
+		cout << "Enter the number of the cell: ";
+	}
 
     int index = choice - 1;
     int row = index / 3;
