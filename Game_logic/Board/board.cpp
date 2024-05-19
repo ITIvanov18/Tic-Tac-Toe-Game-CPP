ebf73cd659d0433e868c259dcacfa2f26512e6a7
ITIvanov18 <ITIvanov18@codingburgas.bg>
2024-05-20 00:42:53 +0300

    Update Game_logic/Board

--- Game_logic/Board/board.cpp
+++ Game_logic/Board/board.cpp
@@ -4,11 +4,13 @@
 using namespace std;
 
 void printBoard(const char board[3][3]) {
+    cout << "━━━━━━━━━━━━━" << endl;
     for (int row = 0; row < 3; row++) {
+        cout << "┃ ";
         for (int col = 0; col < 3; col++) {
-            cout << board[row][col] << ' ';
+            cout << board[row][col] << " ┃ ";
         }
-        cout << endl;
+        cout << "\n━━━━━━━━━━━━━" << endl;
     }
 }
 
